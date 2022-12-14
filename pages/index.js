f7a41d94cec0f497b17f0cfda7357bd2f4f73d0b
Jason Fuhrer <jf.33@hotmail.com>
2022-12-14 08:32:12 -0500

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <body>
-      {/* Header */}
-          {/* Logo */}
-          {/* Nav */}
-
-      {/* Main */}
-          {/* Hero Image */}
-          {/* Examples */}
-      
-      {/* Footer */}
-          {/* Contact info */}
-          {/* FAQ */}
+      Hello
     </body>
   )
   
