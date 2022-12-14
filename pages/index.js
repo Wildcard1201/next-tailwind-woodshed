1424b7bf999e0ae57a128e9a6359652212796893
Jason Fuhrer <jf.33@hotmail.com>
2022-12-14 08:46:55 -0500

    components, styles

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,35 @@
 
 export default function Home() {
   return (
-    <body>
-      Hello
-    </body>
+    <div className='h-screen border-4 border-red-400 bg-green-400'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   )
   
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>This is the header</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <main>
+      This is the main part
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      This is the footer
+    </footer>
+  )
 }
