7941c80c6797de065dc986bb76b61378267a6827
Jason Fuhrer <jf.33@hotmail.com>
2023-01-09 23:44:10 -0500

    fix missing end quotes

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,6 +1,6 @@
 export default function HeroComponent() {
     return (
-        <div className="bg-black">
+        <div className="bg-[url('https://images.unsplash.com/photo-1608613304810-2d4dd52511a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')]">
             This is the Hero Component
         </div>
     )
