92c0ed739dd92f5ec64cfccd2aef573496837026
Jason Fuhrer <jf.33@hotmail.com>
2023-01-10 08:41:30 -0500

    move image to public dir

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,6 +1,6 @@
 export default function HeroComponent() {
     return (
-        <div className="bg-[url('./media/hero-lathe.jpeg')]">
+        <div className="bg-[url('hero-lathe.jpeg')]">
             This is the Hero Component
         </div>
     )
