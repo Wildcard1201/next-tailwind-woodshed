bce35cfc3eb29ca345b1f4e36310f490b97ffece
Jason Fuhrer <jf.33@hotmail.com>
2023-01-11 08:40:18 -0500

    remove image component

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-
 export default function HeroComponent() {
     return (
         <div className="bg-[url('/img/hero-lathe.jpeg')]">
             This is the Hero Component
-            <Image src="/img/hero-lathe.jpeg" alt="lathe image" width="128" height="100" />
         </div>
     )
 }
