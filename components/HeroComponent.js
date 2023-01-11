f4dabe75c9d75759609c4081ff29695c399addb1
Jason Fuhrer <jf.33@hotmail.com>
2023-01-11 08:31:33 -0500

    verify image path via Image component

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,7 +1,10 @@
+import Image from 'next/image'
+
 export default function HeroComponent() {
     return (
         <div >
             This is the Hero Component
+            <Image src="/img/hero-lathe.jpeg" alt="lathe image" width="128" height="100" />
         </div>
     )
 }
