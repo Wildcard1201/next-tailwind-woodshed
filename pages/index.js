28eebc3c9f408361a0d87c2cacd591e85f17e41a
Jason Fuhrer <jf.33@hotmail.com>
2023-01-11 09:01:03 -0500

    logo bg cover

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ function Footer() {
 
 function Logo() {
   return (
-    <div className="border-2 border-black-400 bg-[url('/img/wood-slab.jpeg')] bg-center rounded-full py-2 px-3 text-slate-50 text-2xl">
+    <div className="border-2 border-black-400 bg-[url('/img/wood-slab.jpeg')] bg-center bg-cover rounded-full py-2 px-3 text-slate-50 text-2xl">
       The Woodshed
     </div>
   )
