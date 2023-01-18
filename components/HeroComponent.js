48bf2dcf201e5f25939597a0827affe50dabd7e1
Jason Fuhrer <jf.33@hotmail.com>
2023-01-17 21:58:37 -0500

    hero bg tweaks

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,6 +1,6 @@
 export default function HeroComponent() {
     return (
-        <div className="bg-[url('/img/hero-lathe.jpeg')] text-white text-center py-10">
+        <div className="bg-[url('/img/hero-lathe.jpeg')] bg-center bg-fill text-white text-center py-10">
             <h1 className="text-6xl text-bold text-shadow">
                 Get Stuff Made From Wood
             </h1>
