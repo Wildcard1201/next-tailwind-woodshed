6bab688a5e8e73b78345c0c52c9872d48ea09b86
Jason Fuhrer <jf.33@hotmail.com>
2023-01-21 23:48:23 -0500

    bigger logo

--- components/header/Logo.js
+++ components/header/Logo.js
@@ -1,6 +1,6 @@
 export default function Logo() {
     return (
-      <div className="border-2 border-black-400 bg-[url('/img/wood-slab.jpeg')] bg-center bg-cover rounded-full py-2 px-3 text-slate-50 text-2xl font-bold text-shadow">
+      <div className="border-2 border-black-400 bg-[url('/img/wood-slab.jpeg')] bg-center bg-cover rounded-full py-6 px-4 text-slate-50 text-3xl font-bold text-shadow shadow-lg">
         The Woodshed
       </div>
     )
