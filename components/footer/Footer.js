e6b8ca26d576d50a5f8bf4ce96b635d5fda49aa0
Jason Fuhrer <jf.33@hotmail.com>
2023-01-23 10:11:11 -0500

    create Footer component and layout plan

---
+++ components/footer/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+      <footer className="bg-yellow-100">
+        {/* Love to hear from you */}
+        {/* Address */}
+        {/* Phone */}
+        {/* Tour Button */}
+        {/* Copyright */}
+      </footer>
+    )
+  }
