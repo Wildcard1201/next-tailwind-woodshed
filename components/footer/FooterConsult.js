3b9a6b78e64c41366e92cf6b84686b2d6e7efeac
Jason Fuhrer <jf.33@hotmail.com>
2023-01-23 10:17:18 -0500

    text

---
+++ components/footer/FooterConsult.js
@@ -0,0 +1,12 @@
+export default function FooterConsult() {
+    return (
+        <div className="text-center text-bold text-white text-shadow p-5">
+            <h2 className="text-5xl">
+                WE'D LOVE TO HEAR FROM YOU
+            </h2>
+            <p className="text-xl">
+                Ready to take the next step? Let our professionals help you realize your dreams.
+            </p>
+        </div>
+    )
+}
