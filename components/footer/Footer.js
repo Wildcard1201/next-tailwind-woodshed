9ed6d89a09ba8ec086d3367d6b4e46d491b6bd32
Jason Fuhrer <jf.33@hotmail.com>
2023-01-23 15:46:53 -0500

    footer contact component

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,13 +1,17 @@
 import FooterConsult from './FooterConsult'
+import FooterContact from './FooterContact'
 
 export default function Footer() {
     return (
       <footer className="bg-yellow-600">
         {/* Love to hear from you */}
         <FooterConsult />
+
         {/* Address */}
         {/* Phone */}
         {/* Tour Button */}
+        <FooterContact />
+
         {/* Copyright */}
       </footer>
     )
