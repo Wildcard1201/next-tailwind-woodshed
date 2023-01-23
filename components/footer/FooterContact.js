a807fdb308741ed7e1372220e6ec0afbbded777b
Jason Fuhrer <jf.33@hotmail.com>
2023-01-23 16:04:29 -0500

    flesh out component

--- components/footer/FooterContact.js
+++ components/footer/FooterContact.js
@@ -1,7 +1,25 @@
 export default function() {
     return (
-        <div>
-            footer contact section
+        <div className="flex justify-around items-center p-5 border-2 border-red-500">
+            <div className="border-2">
+                <p>1440 Oak Hill Road</p>
+                <p>Whiterun, OH 44523</p>
+                <br></br>
+                <p>Showroom Hours 8-4 Mon-Fri</p>
+
+            </div>
+
+            <div className="border-2">
+                <p>1-800-555-2525</p>
+                <br></br>
+                <p>woodrow@thewoodshed.com</p>
+            </div>
+
+            <div>
+                <button className="border-4 text-white text-bold text-2xl border-green-500 py-2 px-4 rounded-lg hover:bg-green-300 hover:text-yellow-600">
+                    Take a tour
+                </button>
+            </div>
         </div>
     )
 }
