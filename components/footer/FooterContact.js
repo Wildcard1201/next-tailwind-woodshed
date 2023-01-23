0e142862189a94bc3121c5d428651d57b32b3ab3
Jason Fuhrer <jf.33@hotmail.com>
2023-01-23 16:12:23 -0500

    change tour button hover effect

--- components/footer/FooterContact.js
+++ components/footer/FooterContact.js
@@ -1,7 +1,7 @@
 export default function() {
     return (
-        <div className="flex justify-around items-center p-5 border-2 border-red-500">
-            <div className="border-2">
+        <div className="flex justify-around items-center p-5">
+            <div className="">
                 <p>1440 Oak Hill Road</p>
                 <p>Whiterun, OH 44523</p>
                 <br></br>
@@ -9,14 +9,14 @@ export default function() {
 
             </div>
 
-            <div className="border-2">
+            <div className="">
                 <p>1-800-555-2525</p>
                 <br></br>
                 <p>woodrow@thewoodshed.com</p>
             </div>
 
             <div>
-                <button className="border-4 text-white text-bold text-2xl border-green-500 py-2 px-4 rounded-lg hover:bg-green-300 hover:text-yellow-600">
+                <button className="text-white text-bold text-2xl py-2 px-4 rounded-lg hover:border-4 hover:border-green-500 border-4 border-transparent">
                     Take a tour
                 </button>
             </div>
