4e234a506f0c76032697ac2ca264e7784bf139d9
Jason Fuhrer <jf.33@hotmail.com>
2023-01-23 10:17:22 -0500

    bg color

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,7 +1,10 @@
+import FooterConsult from './FooterConsult'
+
 export default function Footer() {
     return (
-      <footer className="bg-yellow-100">
+      <footer className="bg-yellow-500">
         {/* Love to hear from you */}
+        <FooterConsult />
         {/* Address */}
         {/* Phone */}
         {/* Tour Button */}
