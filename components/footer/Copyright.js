aafec1fbb5d8fd08989e0f16718b7863123cb96e
Jason Fuhrer <jf.33@hotmail.com>
2023-01-24 09:44:34 -0500

    copyright border and padding

--- components/footer/Copyright.js
+++ components/footer/Copyright.js
@@ -1,6 +1,6 @@
 export default function() {
     return (
-        <h4 className="text-center">
+        <h4 className="text-center py-4 border-t-2">
             Copyright Woodrow's Woodshed 2023
         </h4>
     )
