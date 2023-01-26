e933775ce8baa2d70d45c4b8ce5bffd2173a54cb
Jason Fuhrer <jf.33@hotmail.com>
2023-01-26 08:50:37 -0500

    add shadow

--- components/misc/Button.js
+++ components/misc/Button.js
@@ -1,6 +1,6 @@
 export default function Button({ name }) {
     return (
-        <button className="bg-green-400 px-5 py-2 mt-5 rounded-xl text-3xl text-shadow font-bold hover:bg-red-600">
+        <button className="bg-green-400 px-5 py-2 mt-5 rounded-xl text-3xl text-shadow shadow-lg font-bold hover:bg-red-600">
             {name}
         </button>
     )
