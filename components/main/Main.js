b9d0765d3e008d943af668b54fdc518103812ac9
Jason Fuhrer <jf.33@hotmail.com>
2023-01-26 08:53:07 -0500

    add a header for popular items section

--- components/main/Main.js
+++ components/main/Main.js
@@ -5,6 +5,9 @@ export default function Main() {
     return (
       <main className="bg-white">
         <HeroComponent />
+        <h2 className="text-6xl text-bold text-center my-10">
+          Popular Items
+        </h2>
         <div className="w-full flex justify-evenly py-5">
           <Card />
           <Card />
